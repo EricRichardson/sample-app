ab9e09d3d28c37597d8ed454fb2224d3b7ad6602
Eric Richardson <eric@blogoferic.com>
2016-03-01 16:20:26 +0000

    Solved all the problems

--- chap-4-exer.rb
+++ chap-4-exer.rb
@@ -1,5 +1,19 @@
+#Problem 1
 def string_shuffle(s)
    s.split('').shuffle.join
  end
- string_shuffle("foobar")
- "oobfra"
+ 
+ 
+#Problem 2 
+ class String
+  def shuffle
+    self.split('').shuffle.join
+  end
+end
+
+# Problem 3
+person1 = { first: "Awesome", last: "Drifter" }
+person2 = { first: "Mom", last: "Mother" }
+person3 = { first: "Kid", last: "Child" }
+
+params = { father: person1, mother: person2, child: person3 }
