bc2f9f7611d93f026f2e5405c126f686f1238cd3
Eric Richardson <eric@blogoferic.com>
2016-03-08 18:20:43 +0000

    Custom colors, no email activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,8 +20,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
+      session[:user_id] = @user.id
       redirect_to root_url
     else
       render 'new'
